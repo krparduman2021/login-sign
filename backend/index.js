23cb8d89469c0f32b36179cc27b71d7da09893bb
krparduman2021 <krparduman2021@gmail.com>
2026-06-30 15:44:03 +0530

    Fix Render deployment

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,30 @@
 // bcrypt dotenv jsonwebtoken express nodemon cors
 
-const express= require('express')
-const app = express()
-const routes = require('./Routes/UserRoutes');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+const routes = require("./Routes/UserRoutes");
+
+// Middleware
 app.use(cors({
-    origin:'*'
-}))
+    origin: "*"
+}));
 
-app.use(express.json())  //body-parser
-// app.use(express.urlencoded({extended:true}));
-app.get("/",(req,res)=>{
-    res.send("Backend is working ")
-})
-app.use('/pages',routes)
+app.use(express.json());
 
-app.listen(8888, "127.0.0.1", () => {
-    console.log("Server is running fine at http://127.0.0.1:8888")
-})
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Backend is working");
+});
+
+// API Routes
+app.use("/pages", routes);
+
+// Render uses process.env.PORT
+const PORT = process.env.PORT || 8888;
+
+// Start Server
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
